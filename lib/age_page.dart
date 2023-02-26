41bed30ca44384f5fa29990798ab6319e91e43ec
akhiltushar <akhiltushar2000@gmail.com>
2023-02-26 15:24:44 +0530

    Third Commit

--- lib/age_page.dart
+++ lib/age_page.dart
@@ -1,5 +1,7 @@
+import 'package:first_task/constant.dart';
+import 'package:first_task/goals_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class Age_page extends StatefulWidget {
   const Age_page({super.key});
@@ -9,8 +11,189 @@ class Age_page extends StatefulWidget {
 }
 
 class _Age_pageState extends State<Age_page> {
+  var _ageSelection = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: BG_color,
+        leading: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+        ),
+      ),
+      backgroundColor: BG_color,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text('Select your gender',
+                  style: GoogleFonts.poppins(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500)),
+              SizedBox(
+                height: 330,
+              ),
+              Column(
+                children: [
+                  //Age Box set 1
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          // tell widget to load again
+                          setState(() {
+                            _ageSelection = "12-29";
+                          });
+                        },
+                        child: Container(
+                          height: 80,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: _ageSelection == "12-29"
+                                ? Colors.blue
+                                : Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text("12-29",
+                                style: GoogleFonts.poppins(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w500)),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          // tell widget to load again
+                          setState(() {
+                            _ageSelection = "30-39";
+                          });
+                        },
+                        child: Container(
+                          height: 80,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: _ageSelection == "30-39"
+                                ? Colors.blue
+                                : Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text("30-39",
+                                style: GoogleFonts.poppins(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w500)),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+                  //Age Box set 2
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          // tell widget to load again
+                          setState(() {
+                            _ageSelection = "40-49";
+                          });
+                        },
+                        child: Container(
+                          height: 80,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: _ageSelection == "40-49"
+                                ? Colors.blue
+                                : Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text("40-49",
+                                style: GoogleFonts.poppins(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w500)),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          // tell widget to load again
+                          setState(() {
+                            _ageSelection = "50+";
+                          });
+                        },
+                        child: Container(
+                          height: 80,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: _ageSelection == "50+"
+                                ? Colors.blue
+                                : Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text("50+",
+                                style: GoogleFonts.poppins(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w500)),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+
+                  //Next Page Button
+                  SizedBox(
+                    height: 40,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      if (_ageSelection.isEmpty) {
+                        // Show msg to select gender
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              "Please select age",
+                            ),
+                          ),
+                        );
+                      } else {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Goal_page()),
+                        );
+                      }
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 50,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Text('Continue',
+                          style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500)),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/goals_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Goal_page extends StatefulWidget {
+  const Goal_page({super.key});
+
+  @override
+  State<Goal_page> createState() => _Goal_pageState();
+}
+
+class _Goal_pageState extends State<Goal_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
